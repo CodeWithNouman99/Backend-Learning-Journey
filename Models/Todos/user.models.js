1df6d8ff1821b20507b677862dc54a8805c3a29b
Nouman Aslam <mrnoumannomi7@gmail.com>
2026-07-02 13:38:19 +0500

    Data Model: Todo App

---
+++ Models/Todos/user.models.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
+
+
+//jo 5th line me ne "User" likha ha jab wo mongodb me ja kr store ho ga tab wo small letters and plural ban jae ga means users bun jae ga this is important point for interview Perspective
